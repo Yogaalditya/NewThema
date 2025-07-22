fb4cd794fe189e0ca95cf0c583bb9a6cf9b7a0d9
Yogaalditya <www.yogaalditya.com@gmail.com>
2025-07-22 10:34:22 +0700

    Fix header layout after rebase

--- resources/views/frontend/website/components/layouts/header.blade.php
+++ resources/views/frontend/website/components/layouts/header.blade.php
@@ -4,36 +4,22 @@
     $isGlobalNavigationEnabled = App\Facades\Plugin::getPlugin('Tempest')->getSetting('global_navigation')
 @endphp
 
-<<<<<<< HEAD
 @if($isGlobalNavigationEnabled)
 <div class="navbar-publisher navbar-container bg-white shadow z-[51] text-gray-800">
     <div class="navbar mx-auto max-w-7xl items-center h-full">
         <div class="navbar-start items-center gap-x-4 w-max">
-=======
-
-@if($isGlobalNavigationEnabled)
-<div class="navbar-publisher navbar-container bg-white shadow z-[51] text-gray-800">
-    <div class="navbar mx-auto max-w-7xl h-full flex items-center justify-between px-6">
-        <div class="flex items-center gap-x-4 min-w-0">
->>>>>>> 3c54a4d (Rework navbar: UI modern, garis horizontal, font custom, dropdown user menu di atas garis, dan perbaikan responsif)
             <x-website::logo :headerLogo="app()->getSite()->getFirstMedia('logo')?->getAvailableUrl(['thumb', 'thumb-xl'])" :headerLogoAltText="app()->getSite()->getMeta('name')" :homeUrl="url('/')"/>
             @if(App\Models\Conference::exists())
                 @livewire(App\Livewire\GlobalNavigation::class)
             @endif
-<<<<<<< HEAD
 
         </div>
         
         <div class="navbar-end ms-auto gap-x-4 hidden lg:inline-flex">
-=======
-        </div>
-        <div class="flex items-center gap-x-4">
->>>>>>> 3c54a4d (Rework navbar: UI modern, garis horizontal, font custom, dropdown user menu di atas garis, dan perbaikan responsif)
             <x-website::navigation-menu :items="$userNavigationMenu" class="text-gray-800" />
         </div>
     </div>
 </div>
-<<<<<<< HEAD
     
 @endif
 
@@ -58,40 +44,6 @@
             </div>
         </div>
         
-=======
-@endif
-
-
-@if(app()->getCurrentConference() || app()->getCurrentScheduledConference())
-    <div id="navbar" class="navbar-container bg-primary sticky top-0 text-white shadow z-50 relative">
-        <div class="py-2 transition-all duration-100" style="background: transparent; min-height:48px;">
-            <div class="navbar mx-auto flex flex-col px-6 min-h-0">
-                <div class="flex items-center justify-between gap-2 min-w-0 w-full">
-                    <div class="flex items-center gap-2 min-w-0">
-                        <x-website::navigation-menu-mobile />
-                        <x-website::logo :headerLogo="$headerLogo"/>
-                    </div>
-                    @if(!$isGlobalNavigationEnabled)
-                    <div class="user-nav flex items-center">
-                        <x-tempest::navigation-menu 
-                            :items="$userNavigationMenu"
-                            class="flex items-center gap-4" />
-                    </div>
-                    @endif
-                </div>
-            </div>
-            <div class="w-full border-b-2 border-[#d7dee1] opacity-70 my-2"></div>
-            <div class="navbar mx-auto flex flex-col px-2">
-                <div class="w-full flex flex-col lg:flex-row lg:items-center lg:justify-between">
-                    <div class="w-full lg:w-auto flex justify-center lg:justify-start">
-                        <div class="hidden lg:flex relative z-10 w-full">
-                            <x-website::navigation-menu :items="$primaryNavigationItems" />
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
->>>>>>> 3c54a4d (Rework navbar: UI modern, garis horizontal, font custom, dropdown user menu di atas garis, dan perbaikan responsif)
     </div>
 @endif
 
